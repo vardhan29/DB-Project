e1c5daf33543f729c80c515c2e0e1756bae18a8e
vdeshpa4 <92765348+vdeshpa4@users.noreply.github.com>
2021-10-31 13:27:15 -0400

    Adding Stored Procedures

--- sql-scripts/Driver-Rating-Procedure.sql
+++ sql-scripts/Driver-Rating-Procedure.sql
@@ -1,8 +1,15 @@
 USE Campus_Eats_Fall2020;
 
+-- Stored Procedure to get the average driver rating based on all the reviews.
+-- Input  : Driver id
+-- Output : Average Rating of the driver
+
+-- Change Delimiter to '//'
 DELIMITER //
+
 CREATE PROCEDURE spGetDriverRating(IN id INT, OUT avg_rating FLOAT)
 BEGIN
+	-- Get average rating for a driver by joining two tables : rating and order
 	SELECT AVG(r.driver_rating)
     INTO avg_rating
     FROM `order` o
@@ -12,7 +19,10 @@ BEGIN
     GROUP BY o.driver_id;
 END //
 
+-- Set Delimiter back to ';'
 DELIMITER ;
 
-call spGetDriverRating(4, @avg_rating);
+-- Call the stored procedure by sending the input and the output variable
+CALL spGetDriverRating(4, @avg_rating);
+-- View that data by using SELECT on the output variable
 SELECT @avg_rating;
