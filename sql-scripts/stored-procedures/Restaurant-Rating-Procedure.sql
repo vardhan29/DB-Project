732c50e2d81b84697cd0b6aeb8f1a0f8486c2d81
vdeshpa4 <92765348+vdeshpa4@users.noreply.github.com>
2021-12-07 19:29:48 -0500

    Procedure to get min, max, avg restaurant rating

--- sql-scripts/stored-procedures/Restaurant-Rating-Procedure.sql
+++ sql-scripts/stored-procedures/Restaurant-Rating-Procedure.sql
@@ -7,11 +7,29 @@ USE Campus_Eats_Fall2020;
 -- Change Delimiter to '//'
 DELIMITER //
 
-CREATE PROCEDURE spGetRestaurantRating(IN id INT, OUT avg_rest_rating FLOAT)
+CREATE PROCEDURE spGetRestaurantRating(IN id INT, OUT avg_rest_rating FLOAT, OUT max_rest_rating FLOAT, OUT min_rest_rating FLOAT)
 BEGIN
 	-- Get average rating for a restaurant by joining two tables : rating and order
 	SELECT AVG(r.restaurant_rating)
     INTO avg_rest_rating
+    FROM `order` o
+    JOIN `rating` r
+    WHERE o.order_id = r.order_id
+    AND o.restaurant_id = id
+    GROUP BY o.restaurant_id;
+    
+	-- Get maximum rating for a restaurant by joining two tables : rating and order
+	SELECT MAX(r.restaurant_rating)
+    INTO max_rest_rating
+    FROM `order` o
+    JOIN `rating` r
+    WHERE o.order_id = r.order_id
+    AND o.restaurant_id = id
+    GROUP BY o.restaurant_id;
+    
+	-- Get minimum rating for a restaurant by joining two tables : rating and order
+	SELECT MIN(r.restaurant_rating)
+    INTO min_rest_rating
     FROM `order` o
     JOIN `rating` r
     WHERE o.order_id = r.order_id
@@ -23,6 +41,8 @@ END //
 DELIMITER ;
 
 -- Call the stored procedure by sending the input and the output variable
-CALL spGetRestaurantRating(4, @avg_rest_rating);
+CALL spGetRestaurantRating(4, @avg_rest_rating, @max_rest_rating, @min_rest_rating);
 -- View that data by using SELECT on the output variable
-SELECT @avg_rest_rating;
+SELECT @avg_rest_rating AS Average_rating, 
+	@max_rest_rating AS Maximum_rating,
+    @min_rest_rating AS Minimum_rating;
